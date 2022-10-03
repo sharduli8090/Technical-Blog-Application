e21468f651af5d7f0dcb78042e24d4d4ac4bdfec
Sharduli Pandey <shardulipandey8090@gmail.com>
2022-10-03 23:19:15 +0530

    Maven Project Created

---
+++ src/main/java/blogapp/controller/HomeController.java
@@ -0,0 +1,38 @@
+package blogapp.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import blogapp.model.Post;
+import java.util.Date;
+
+@controller
+public class HomeController{
+
+    @RequestMapping("/")
+    public string getAllPosts(Model model){
+
+        ArrayList<Post> posts = new ArrayList<>();
+
+        Post post1 = new Post();
+        post1.setTitle("Post 1");
+        post1.setBody("Post Body 1");
+        post1.setDate(new Date());
+        
+        Post post2 = new Post();
+        post2.setTitle("Post 2");
+        post2.setBody("Post Body 2");
+        post2.setDate(new Date());
+        
+        Post post3 = new Post();
+        post3.setTitle("Post 3");
+        post3.setBody("Post Body 3");
+        post3.setDate(new Date());
+
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post3);
+
+        model.addAttribute("posts",posts);
+        return "index";
+    }
+  }
